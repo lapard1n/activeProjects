1c52aa9f9b6ccf6b311d23af250e7147a52c11a1
lapard1n <rictumarcus@gmail.com>
2024-04-18 01:10:23 +0300

    new build

--- firstToDo/js/main.js
+++ firstToDo/js/main.js
@@ -1,31 +1,46 @@
 "use strict"
 
 const iputBox = document.getElementById('input-box');
-const inputButton = document.getElementById('input-button');
+const inputPublish = document.getElementById('input-publish');
 const listContainer = document.getElementById('list-container');
-const taskPlaceholder = document.getElementById('todo-app-placeholder');
+const listInfo = document.getElementById('tasks-info');
+const numberCounter = listInfo.querySelector('.todo-app__counter');
+const placeHolder = listInfo.querySelector('.todo-app__placeholder');
+
 const taskPrototype = document.getElementById('task-prototype');
 taskPrototype.removeAttribute('id');
-taskPrototype.remove();
+listContainer.innerHTML = '';
 
-// СПОСОБ СОЗДАНИЯ ЭЛЕМЕНТА ЧЕРЕЗ ДОБАВЛЕНИЕ HTML РАЗМЕТКИ
+// СОХРАНЕНИЕ ДАННЫХ В localstorage:
+function saveLocal() {
+  localStorage.setItem('tasks', listContainer.innerHTML);
+  localStorage.setItem('counter', numberCounter.innerHTML);
+  localStorage.setItem('placeholder', placeHolder.textContent);
+}
+function showLocal() {
+  listContainer.innerHTML = localStorage.getItem('tasks');
+  numberCounter.innerHTML = localStorage.getItem('counter');
+  placeHolder.textContent = localStorage.getItem('placeholder');
+}
+showLocal();
+
+// СОЗДАНИЕ ЭЛЕМЕНТА ЧЕРЕЗ ДОБАВЛЕНИЕ HTML РАЗМЕТКИ:
 function createTask(str) {
+  const taskText = document.querySelector('.task p');
   const task = `
     <article class="task">
       <p></p>
       <div class="task__panel">
         <img class="task__panel-button" src="./img/editDark.svg" alt="Button to change the task.">
-        <span></span>
         <img class="task__panel-button" src="./img/trashDark.svg" alt="Button for deleting a task.">
       </div>
     </article>`;
-  listContainer.insertAdjacentHTML('afterbegin', task);
 
-  const taskText = document.querySelector('.task p');
+  listContainer.insertAdjacentHTML('afterbegin', task);
   taskText.insertAdjacentText('afterbegin', str);
 }
 
-// СПОСОБ СОЗДАНИЯ ЭЛЕМЕНТА ЧЕРЕЗ КОПИРОВАНИЯ УЗЛА
+// СОЗДАНИЕ ЭЛЕМЕНТА ЧЕРЕЗ КОПИРОВАНИЯ УЗЛА:
 function cloneTask(str) {
   const task = taskPrototype.cloneNode(true);
   const taskText = task.querySelector('p');
@@ -33,7 +48,7 @@ function cloneTask(str) {
   listContainer.prepend(task);
 }
 
-// РЕГУЛЯРКА И ФУНКЦИЯ ДЛЯ ПРОВЕРКИ ТЕКСТА В ПОЛЕ input
+// РЕГУЛЯРКА И ФУНКЦИЯ ПРОВЕРКИ ТЕКСТА В ПОЛЕ input:
 const regExpSpaceCheker = /\S/;
 regExpSpaceCheker.test('iputBox.value');
 function splitSpace(str) {
@@ -41,35 +56,129 @@ function splitSpace(str) {
   return str ? true : false;
 }
 
-// СЛУШАТЕЛЬ СОБЫТИЙ ДЛЯ ПРОВЕРКИ ВВОДА В ПОЛЕ input
+// СЛУШАТЕЛЬ СОБЫТИЙ ПРОВЕРКИ ВВОДА В ПОЛЕ input:
 iputBox.addEventListener('input', () => {
-  if ((iputBox.value.length === 0 || !splitSpace(iputBox.value)) && !inputButton.hasAttribute('disabled')) {
-    inputButton.setAttribute('disabled', 'disabled');
+  if ((iputBox.value.length === 0 || !splitSpace(iputBox.value)) && !inputPublish.hasAttribute('disabled')) {
+    inputPublish.setAttribute('disabled', 'disabled');
   }
 
-  if (splitSpace(iputBox.value) && inputButton.hasAttribute('disabled')) {
-    inputButton.removeAttribute('disabled');
+  if (splitSpace(iputBox.value) && inputPublish.hasAttribute('disabled')) {
+    inputPublish.removeAttribute('disabled');
   }
 })
 
-// ФУНКЦИЯ ДЛЯ ДОБАВЛЕНИЯ ТЕКСТА ИЗ input
+// ДОБАВЛЕНИЕ ТЕКСТА ИЗ input:
 function addingTask(event) {
-  // СПОСОБЫ УДАЛЕНИЯ ВНЕШНИХ ПРОБЕЛОВ У СТРОКИ:
-  const regExpDeletionSpaces = (iputBox.value).replace(/^\s+|\s+$/g, '');
-  const newString = (iputBox.value).trim();
-
   if (event.type === 'click' || (event.keyCode === 13 || event.code === 'Enter')) {
-    if (!inputButton.hasAttribute('disabled')) {
+    if (!inputPublish.hasAttribute('disabled')) {
       cloneTask(iputBox.value);
       iputBox.value = '';
     }
 
-    if ((iputBox.value.length === 0 || !splitSpace(iputBox.value)) && !inputButton.hasAttribute('disabled')) {
-      inputButton.setAttribute('disabled', 'disabled');
+    if ((iputBox.value.length === 0 || !splitSpace(iputBox.value)) && !inputPublish.hasAttribute('disabled')) {
+      inputPublish.setAttribute('disabled', 'disabled');
     }
-    taskPlaceholder.textContent = "Keep it up!";
+
+    personalObserver();
+    saveLocal();
   }
 }
-inputButton.addEventListener('click', addingTask);
+
+// СЛУШАТЕЛИ СОБЫТИЙ ДОБАВЛЕНИЯ ТЕКСТА В todo-app__list:
+inputPublish.addEventListener('click', addingTask);
 iputBox.addEventListener('keydown', addingTask);
-taskPlaceholder.onclick = () => { iputBox.focus() }
+
+//ФУНКЦИЯ ОБРАБОТКИ СОДЕРЖИМОГО todo-app__info:
+function personalObserver() {
+  let clearButton = listInfo.querySelector('.todo-app__clear');
+  let tasksQuantity = listInfo.querySelectorAll('.todo-app__counter-value');
+  let numberChecked = listContainer.querySelectorAll('.task.checked');
+
+  if (listContainer.childNodes.length === 0) {
+    placeHolder.textContent = 'Add your task right here~';
+    clearButton.setAttribute('disabled', 'disabled');
+  }
+  if (listContainer.childNodes.length > 0) {
+    placeHolder.textContent = 'Keep it up!';
+    clearButton.removeAttribute('disabled');
+  }
+
+  tasksQuantity[0].textContent = `tasks: ${listContainer.childNodes.length}`;
+  tasksQuantity[1].textContent = `checked: ${numberChecked.length}`;
+}
+
+// ДЕЛЕГИРОВАНИЕ СОБЫТИЙ В todo-app__list:
+listContainer.addEventListener('click', (e) => {
+  let eventTarget = e.target;
+  let taskElement = eventTarget.closest('.task');
+
+  if (eventTarget.hasAttribute('data-delete')) {
+    taskElement.remove();
+  }
+
+  if (eventTarget.hasAttribute('data-change')) {
+    console.log('change');
+  }
+
+  if (eventTarget.tagName === 'P') {
+    if (!taskElement.classList.contains('checked')) {
+      taskElement.classList.add('checked');
+    } else {
+      taskElement.classList.remove('checked');
+    }
+  }
+
+  personalObserver();
+  saveLocal();
+})
+
+// ДЕЛЕГИРОВАНИЕ СОБЫТИЙ ДЛЯ todo-app__info:
+listInfo.addEventListener('click', (e) => {
+  let eventTarget = e.target;
+
+  if (eventTarget.matches('.todo-app__placeholder')) {
+    iputBox.focus();
+  }
+
+  if (eventTarget.matches('.todo-app__clear') && !eventTarget.hasAttribute('disabled')) {
+    listContainer.innerHTML = '';
+
+    personalObserver();
+    saveLocal();
+  }
+})
+
+const clearButtonNew = listInfo.querySelector('.todo-app__clear');
+clearButtonNew.addEventListener('mouseenter', (event) => {
+
+  if (event.target === clearButtonNew && !event.target.hasAttribute('disabled')) {
+    let clearLine = document.createElement('span');
+    clearLine.classList.add('line-animation');
+    clearLine.style.animation = 'line-animation 500ms forwards';
+    clearButtonNew.append(clearLine);
+  }
+})
+listInfo.querySelector('.todo-app__clear').addEventListener('mouseleave', (event) => {
+  if (event.target === clearButtonNew) {
+    let clearLine = clearButtonNew.querySelectorAll('span');
+    clearLine.forEach(element => {
+      element.style.animation = 'reverse-line 500ms forwards';
+      setTimeout(() => { element.remove(); }, 500);
+    })
+  }
+})
+
+/* USELESS SNIPPETS:
+const regExpDeletionSpaces = (iputBox.value).replace(/^\s+|\s+$/g, '');
+const newString = (iputBox.value).trim();
+
+let counters = document.querySelectorAll('.todo-app__counter-value');
+let textArray = Array.from(counters).map(el => {
+  return el.textContent;
+})
+
+textArray.forEach((element, index) => {
+  let nameElement = element.match(/\b[a-zA-Z]+\b/gi).join('') + '_' + index;
+  localStorage.setItem(`${nameElement}`, element);
+})
+*/
--- firstToDo/js/test.js
+++ firstToDo/js/test.js
@@ -1,4 +1,10 @@
-function splitSpace(str) {
+// УДАЛЕНИЕ ВНЕШНИХ ПРОБЕЛОВ В СТОКЕ:
+const input = '   Hello, world!     ';
+input.replace(/^\s+|\s+$/g, '')
+input.trim()
+
+// ПРОВЕРКА СТРОКИ НА ЗАПОЛНЕННОСТЬ:
+function splitSpace00(str) {
   str = str.split(' ').join('');
   if (str) {
     console.log(true, 'long');
@@ -6,16 +12,56 @@ function splitSpace(str) {
     console.log(false, 'long');
   }
 }
-
 function splitSpace01(str) {
   str = str.split(' ').join('');
   return str ? console.log(str, true, 'short') : console.log(str, false, 'short');
 }
 
-// const input = '   Hello, world!     ';
-// console.log(
-//   input.replace(/^\s+|\s+$/g, '')
-// );
-// console.log(input.trim());
+// МЕТОДЫ УДАЛЕНИЯ ПРОБЕЛОВ В МУЛЬТИСТРОКЕ:
+let someText = `
 
-splitSpace01('     Hello world boy ')
+          <span class="todo-app__counter-value">tasks: 0</span>
+          <span class="todo-app__counter-value">checked: 0</span>
+
+          `;
+console.log(someText.replace(/^\s+/gim, ''));
+console.log(someText.replace(/^\s+|\s+$/gim, ''));
+console.log(someText.split('\n').map((el, i) => el.match(/\S+/g) ? el.trim() : el = '').join('\n').trim());
+
+// ФУНКЦИЯ ПЕРЕВОРОТА СЛОВ ОТ 5 СИМВОЛОВ В СТРКОЕ:
+function spinWords00(s) {
+  return s.split(' ').map(w => w.length > 4 ? w.split('').reverse().join('') : w).join(' ');
+}
+function spinWords01(string) {
+  return string.replace(/\w{5,}/gim, (el) => { return el.split('').reverse().join('') })
+}
+
+// ФУНКЦИЯ ПРИМЕРА СВИТЧА:
+function likes00(names) {
+  let newStr = '';
+  if (names.length === 0) {
+    return 'no one likes this';
+  }
+  if (names.length === 1) {
+    newStr = names[0] + ' likes this';
+  }
+  if (names.length === 2) {
+    newStr = names.join(' and ') + ' like this';
+  }
+  if (names.length === 3) {
+    newStr = names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+  }
+  if (names.length > 3) {
+    newStr = names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+  }
+  return newStr;
+}
+function likes01(names) {
+  switch (names.length) {
+    case 0: return `no one likes this`;
+    case 1: return `${names[0]} likes this`;
+    case 2: return `${names[0]} and ${names[1]} like this`;
+    case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default: return `${names[0]}, ${names[1]} and ${(names.length - 2)} others like this`;
+  }
+}
